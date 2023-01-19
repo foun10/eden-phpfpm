d78dd42377ca53a0bb548bff8e30def3b921f0ab
Alexander Schneider <schneider@foun10.de>
2023-01-19 09:06:35 +0100

    Adjust execution order

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 if [[ ! -f "/eden-scripts/init.lock" ]]; then
-    touch "/eden-scripts/init.lock"
-    chmod 777 "/eden-scripts/init.lock"
-
     if [[ -f "/eden-scripts/init.sh" ]]; then
         bash "/eden-scripts/init.sh"
     fi
+
+    touch "/eden-scripts/init.lock"
+    chmod 777 "/eden-scripts/init.lock"
 fi
 
 if [[ -f "/eden-scripts/always.sh" ]]; then
