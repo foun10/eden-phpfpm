cc15828567b5c34a999173878bd66a365ccf60a6
Alexander Schneider <schneider@foun10.de>
2025-11-17 12:19:22 +0100

    Adjust packages

--- packages/install_packages.sh
+++ packages/install_packages.sh
@@ -10,14 +10,16 @@ apt-get dist-upgrade --fix-missing -y
 echo "Install default packages"
 apt-get install -y $(paste -s -d ' ' ${TMP_PACKAGE_PATH}packages_default)
 
-
 # Detect version
 . /etc/os-release
 
 if [[ -f ${TMP_PACKAGE_PATH}packages_${VERSION_ID} ]]; then
   echo "Install packages for version ${VERSION_ID}"
   apt-get install -y $(paste -s -d ' ' ${TMP_PACKAGE_PATH}packages_${VERSION_ID})
+elseif [[ VERSION_ID < 10 ]]; them
+  echo "No specified packages for version ${VERSION_ID} using default packages for version lower than 10"
+  apt-get install -y $(paste -s -d ' ' ${TMP_PACKAGE_PATH}packages_lower_10)
 else
-  echo "No specified packages for version ${VERSION_ID} found using fallback packages"
-  apt-get install -y $(paste -s -d ' ' ${TMP_PACKAGE_PATH}packages_fallback)
+  echo "No specified packages for version ${VERSION_ID} using default packages for version higher than 10"
+  apt-get install -y $(paste -s -d ' ' ${TMP_PACKAGE_PATH}packages_higher_10)
 fi
